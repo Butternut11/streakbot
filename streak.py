80b47a35bda3d8b6f650f48eca0c8fba9c565d89
Zach Mohan <zmohan@wustl.edu>
2018-10-04 13:57:35 -0500

    updated security

--- streak.py
+++ streak.py
@@ -12,29 +12,29 @@ bot = Bot(command_prefix=BOT_PREFIX)
 streaks = {}
 
 pots = collections.defaultdict(list)
-totalpot = collections.defaultdict(list)
+total_pot = collections.defaultdict(list)
 
 
 @bot.command()
 @commands.has_role("Host")
 async def b(ctx, member: discord.Member, bet: float):
-    global totalpot
+    global total_pot
     payout = round(bet*1.8, 2)
-    newpot = round(bet*2, 2)
-    tmpstr = pots[ctx.author.id]
-    formattedString = f'<@{member.id}>'
+    new_pot = round(bet*2, 2)
+    temp_str = pots[ctx.author.id]
+    formatted_string = f'<@{member.id}>'
     count = 0
-    betsExist = False
-    for bets in tmpstr:
-        if formattedString in bets:
-            betsExist = True
-            newpayout = float(pots[ctx.author.id][count].split()[4]) + float(payout)
-            newbet = float(pots[ctx.author.id][count].split()[2]) + float(bet)
-            pots[ctx.author.id][count] = f'<@{member.id}> **Bet**: {newbet} **Payout**: {newpayout}'
+    bets_exist = False
+    for bets in temp_str:
+        if formatted_string in bets:
+            bets_exist = True
+            new_payout = float(pots[ctx.author.id][count].split()[4]) + float(payout)
+            new_bet = float(pots[ctx.author.id][count].split()[2]) + float(bet)
+            pots[ctx.author.id][count] = f'<@{member.id}> **Bet**: {new_bet} **Payout**: {new_payout}'
         count += 1
-    if not betsExist:
+    if not bets_exist:
         pots[ctx.author.id].append(f'<@{member.id}> **Bet**: {bet} **Payout**: {payout}')
-    totalpot[ctx.author.id].append(f'{newpot}')
+    total_pot[ctx.author.id].append(f'{new_pot}')
     await ctx.send(embed=discord.Embed(
         title='Confirmation',
         description='```Added to the pot```'
@@ -43,10 +43,10 @@ async def b(ctx, member: discord.Member, bet: float):
 
 @bot.command(name="potreset")
 @commands.has_role("Host")
-async def potreset(ctx):
-    global totalpot
+async def pot_reset(ctx):
+    global total_pot
     user = ctx.author.id
-    totalpot.pop(user, None)
+    total_pot.pop(user, None)
     pots.pop(user, None)
     await ctx.send(embed=discord.Embed(
         title='Pot Reset',
@@ -56,7 +56,7 @@ async def potreset(ctx):
 
 @bot.command(name="pot")
 async def pot(ctx, member: discord.Member = None):
-    userpot = 0.0
+    user_pot = 0.0
     if member is None:
         user = ctx.author.id
     else:
@@ -72,29 +72,29 @@ async def pot(ctx, member: discord.Member = None):
 
     if msg == "":
         msg = "User has no pot"
-    for numbers in totalpot[user]:
-        userpot += float(numbers)
+    for numbers in total_pot[user]:
+        user_pot += float(numbers)
     await ctx.send(embed=discord.Embed(
         title='Pot',
-        description=(f'{msg} \n ```Total pot: {userpot}```')
+        description=f'{msg} \n ```Total pot: {user_pot}```'
     ))
 
 
 @bot.command(name="win")
 @commands.has_role("Host")
 async def win(ctx):
-    
+
     if str(ctx.message.author.id) in streaks:
-    
+
         streaks[str(ctx.message.author.id)].insert(0, "<:win:489151982918172723>")
     else:
         streaks[str(ctx.message.author.id)] = []
         streaks[str(ctx.message.author.id)].append("<:win:489151982918172723>")
 
-    userpot = 0.0
+    user_pot = 0.0
     user = ctx.message.author.id
     msg = ""
-    global totalpot
+    global total_pot
 
     if user in pots:
         for bets in pots[user]:
@@ -104,42 +104,41 @@ async def win(ctx):
 
     if msg == "":
         msg = "User has no pot"
-    for numbers in totalpot[user]:
-        userpot += float(numbers)
+    for numbers in total_pot[user]:
+        user_pot += float(numbers)
     await ctx.send(embed=discord.Embed(
         title='Pot',
-        description=f'{msg} \n ```Total pot: {userpot}```'
+        description=f'{msg} \n ```Total pot: {user_pot}```'
     ))
 
     pots.pop(user, None)
-    totalpot.pop(user, None)
+    total_pot.pop(user, None)
 
     user = ctx.message.author
     embed = discord.Embed()
     wins = 0
     lossed = 0
     total = "0"
-    messageToSend = ""
+    message_to_send = ""
     if str(user.id) in streaks:
         for streakEmote in streaks[str(user.id)]:
-            if not messageToSend == "":
-                messageToSend = messageToSend +","+ streakEmote
+            if not message_to_send == "":
+                message_to_send = message_to_send + "," + streakEmote
             else:
-                messageToSend = streakEmote
+                message_to_send = streakEmote
             if streakEmote == "<:loss:489151983018704896>":
-                lossed = lossed +1
+                lossed = lossed + 1
             elif streakEmote == "<:win:489151982918172723>":
-                wins = wins +1
-        
-            
+                wins = wins + 1
+
         total = wins + lossed
     embed.set_author(name=user.name, icon_url=user.avatar_url)
         
     embed.set_footer(text="WINS: "+str(wins)+"		LOSS: "+str(lossed)+"		TOTAL DUELS: "+str(total))
-    if messageToSend == "":
-        messageToSend = "Nothing"
+    if message_to_send == "":
+        message_to_send = "Nothing"
         
-    embed.add_field(name="All Streaks", value=messageToSend)
+    embed.add_field(name="All Streaks", value=message_to_send)
         
     await ctx.message.channel.send(embed=embed)
 
@@ -157,8 +156,8 @@ async def loss(ctx):
 
     user = ctx.message.author.id
     msg = ""
-    userpot = 0.0
-    global totalpot
+    user_pot = 0.0
+    global total_pot
 
     if user in pots:
         for bets in pots[user]:
@@ -168,44 +167,44 @@ async def loss(ctx):
 
     if msg == "":
         msg = "User has no pot"
-    for numbers in totalpot[user]:
-        userpot += float(numbers)
+    for numbers in total_pot[user]:
+        user_pot += float(numbers)
     await ctx.send(embed=discord.Embed(
         title='Pot',
-        description=(f'{msg} \n ```Total pot: {userpot}```')
+        description=f'{msg} \n ```Total pot: {user_pot}```'
     ))
 
     pots.pop(user, None)
-    totalpot.pop(user, None)
+    total_pot.pop(user, None)
 
     user = ctx.message.author
     embed = discord.Embed()
     wins = 0
     lossed = 0
     total = "0"
-    messageToSend = ""
+    message_to_send = ""
     if str(user.id) in streaks:
         for streakEmote in streaks[str(user.id)]:
-            if not messageToSend == "":
-                messageToSend = messageToSend +","+ streakEmote
+            if not message_to_send == "":
+                message_to_send = message_to_send + "," + streakEmote
             else:
-                messageToSend = streakEmote
+                message_to_send = streakEmote
             if streakEmote == "<:loss:489151983018704896>":
-                lossed = lossed +1
+                lossed = lossed + 1
             elif streakEmote == "<:win:489151982918172723>":
-                wins = wins +1
-        
-            
+                wins = wins + 1
+
         total = wins + lossed
     embed.set_author(name=user.name, icon_url=user.avatar_url)
         
     embed.set_footer(text="WINS: "+str(wins)+"		LOSS: "+str(lossed)+"		TOTAL DUELS: "+str(total))
-    if messageToSend == "":
-        messageToSend = "Nothing"
+    if message_to_send == "":
+        message_to_send = "Nothing"
         
-    embed.add_field(name="All Streaks", value=messageToSend)
+    embed.add_field(name="All Streaks", value=message_to_send)
         
     await ctx.message.channel.send(embed=embed)
+
 
 @bot.command(name="fresh")
 @commands.has_role("Host")
@@ -223,27 +222,26 @@ async def fresh(ctx):
     wins = 0
     lossed = 0
     total = "0"
-    messageToSend = ""
+    message_to_send = ""
     if str(user.id) in streaks:
         for streakEmote in streaks[str(user.id)]:
-            if not messageToSend == "":
-                messageToSend = messageToSend +","+ streakEmote
+            if not message_to_send == "":
+                message_to_send = message_to_send + "," + streakEmote
             else:
-                messageToSend = streakEmote
+                message_to_send = streakEmote
             if streakEmote == "<:loss:489151983018704896>":
-                lossed = lossed +1
+                lossed = lossed + 1
             elif streakEmote == "<:win:489151982918172723>":
-                wins = wins +1
-        
-            
+                wins = wins + 1
+
         total = wins + lossed
     embed.set_author(name=user.name, icon_url=user.avatar_url)
         
     embed.set_footer(text="WINS: "+str(wins)+"		LOSS: "+str(lossed)+"		TOTAL DUELS: "+str(total))
-    if messageToSend == "":
-        messageToSend = "Nothing"
+    if message_to_send == "":
+        message_to_send = "Nothing"
         
-    embed.add_field(name="All Streaks", value=messageToSend)
+    embed.add_field(name="All Streaks", value=message_to_send)
         
     await ctx.message.channel.send(embed=embed)
     
@@ -253,75 +251,71 @@ async def fresh(ctx):
 async def streakreset(ctx):
     
     streaks[str(ctx.message.author.id)] = []
-    totalpot[ctx.message.author.id] = ""
+    total_pot[ctx.message.author.id] = []
+
 
 async def is_zach(ctx):
     return ctx.author.id == 209081432956600320
+
 
 @bot.command(name="zach")
 @commands.check(is_zach)
 async def zach(ctx):
     await ctx.send("Zach is the best cashier in StakeLand")
 
-@bot.command(name="fuckbarry")
-async def fuckbarry(ctx):
-    await ctx.send("Barry has a small peepee")
 
 @bot.command(name="slowbarry")
 @commands.has_role("Super Nerdy")
 async def slowbarry(ctx):
     await ctx.send("```Deep Breaths Barry, Slow down```")
 
+
 @bot.command(name="stop")
 @commands.has_role("Super Nerdy")
 async def stop(ctx):
     await ctx.send("```I think it would be a good idea for you to stop now. ```")
 
+
 @bot.command(name="streak")
-async def streak(ctx, member : discord.Member = None):
-    user = None
-    
+async def streak(ctx, member: discord.Member = None):
+
     if member is None: 
         user = ctx.message.author
     else:
         user = member
-    
-    if not user is None:
-        validHost = False
-        roles = user.roles
-        for role in roles:
-            if str(role) == "Host":
-                validHost = True
-        
-        if validHost:
-            embed = discord.Embed()
-            wins = 0
-            lossed = 0
-            total = "0"
-            messageToSend = ""
-            if str(user.id) in streaks:
-                for streakEmote in streaks[str(user.id)]:
-                    if not messageToSend == "":
-                        messageToSend = messageToSend +","+ streakEmote
-                    else:
-                        messageToSend = streakEmote
-                    if streakEmote == "<:loss:489151983018704896>":
-                        lossed = lossed +1
-                    elif streakEmote == "<:win:489151982918172723>":
-                        wins = wins +1
-                
-                    
-                total = wins + lossed
-            embed.set_author(name=user.name, icon_url=user.avatar_url)
-                
-            embed.set_footer(text="WINS: "+str(wins)+"		LOSS: "+str(lossed)+"		TOTAL DUELS: "+str(total))
-            if messageToSend == "":
-                messageToSend = "Nothing"
-                
-            embed.add_field(name="All Streaks", value=messageToSend)
-                
-            await ctx.message.channel.send(embed=embed)
 
-# bot.run('NDc5MDkwODg1OTk3NjI1MzY0.Dn67Fg.iRWIgTNP7f1mF0zu5uLHnVrpPwc')
-# bot.run('NDg5MzczODU1OTQxNTI1NTA1.Dnp0eA.3IBn8bvOZJ5ZJu2O2EAGAQJRXb0')
+    valid_host = False
+    roles = user.roles
+    for role in roles:
+        if str(role) == "Host":
+            valid_host = True
+
+    if valid_host:
+        embed = discord.Embed()
+        wins = 0
+        lossed = 0
+        total = "0"
+        message_to_send = ""
+        if str(user.id) in streaks:
+            for streakEmote in streaks[str(user.id)]:
+                if not message_to_send == "":
+                    message_to_send = message_to_send + "," + streakEmote
+                else:
+                    message_to_send = streakEmote
+                if streakEmote == "<:loss:489151983018704896>":
+                    lossed = lossed + 1
+                elif streakEmote == "<:win:489151982918172723>":
+                    wins = wins + 1
+
+            total = wins + lossed
+        embed.set_author(name=user.name, icon_url=user.avatar_url)
+
+        embed.set_footer(text="WINS: "+str(wins)+"		LOSS: "+str(lossed)+"		TOTAL DUELS: "+str(total))
+        if message_to_send == "":
+            message_to_send = "Nothing"
+
+        embed.add_field(name="All Streaks", value=message_to_send)
+
+        await ctx.message.channel.send(embed=embed)
+
 bot.run(TOKEN)
